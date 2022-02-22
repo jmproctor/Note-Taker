e001a0a8218375ec5856a6581fdb7bb6b4216019
JMP <jmproctor14@live.com>
2022-02-22 13:59:43 -0600

    added delete function and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# note-taker
+# Note Taker
+
+This app is a simple note taker app that allows the user to create and delete a note with a title and text.
+
+## User Story
+
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+Link to repo: https://github.com/jmproctor/note-taker
+
+link to Deployed app: https://jmp-note-taker.herokuapp.com/
--- server.js
+++ server.js
@@ -13,8 +13,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// pulls notes from db
 
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(htmlRoute, 'notes.html'));
+});
 
+app.get('/api/notes', function(req, res) {
+    res.sendFile(path.join(apiRoute, 'db.json'))
+    return res.body
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(htmlRoute, 'index.html'));
+});
+
+// saves content from notes created by user
+
+app.post('/api/notes', function(req, res) {
+    var savedNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+    var newNote = req.body;
+
+    var noteId = (savedNotes.length).toString();
+    newNote.id = noteId;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+
+    res.json(savedNotes);
+});
+
+// allows user to delete previous notes
+
+app.delete('/api/notes/:id', function(req, res) {
+    var savedNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+    var noteID = req.params.id;
+    var newID = 0;
+
+    savedNotes = savedNotes.filter(currentNote => {
+        return currentNote.id != noteID;
+    })
+
+    for (currentNote of savedNotes) {
+        currentNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+    return res.json(savedNotes);
+});
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}!`);
